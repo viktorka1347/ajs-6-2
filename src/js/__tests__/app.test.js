7cd832966cf22cfb65f07d3549e66bfe13ee6c81
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-10-22 15:49:15 +0300

    main
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,42 @@
+import getSpecial from '../app';
+
+test('Функция getSpecial должна правильно извлекать массив', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+
+  const special = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+
+  expect(getSpecial(character)).toEqual(special);
+});
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+export default function getSpecial(obj) {
+    return obj.special.map((item) => {
+      const {
+        id, name, icon, description = 'Описание недоступно',
+      } = item;
+      return {
+        id, name, icon, description,
+      };
+    });
+  }
